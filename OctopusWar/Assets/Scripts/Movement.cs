823b4d265542929385cd93c245b275e690b7c262
literallyme1 <kde7891@naver.com>
2024-02-19 14:26:24 +0900

    Fifth Commit
    Fifth Commit

--- OctopusWar/Assets/Scripts/Movement.cs
+++ OctopusWar/Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -9,18 +10,36 @@ public class Movement : MonoBehaviour
     public Joystick joystick;
 
     Rigidbody rb;
+    Vector3 moveVec;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
-        rb.AddForce(direction * speed * Time.deltaTime, ForceMode.VelocityChange);
+        // 1. Input Value
+        float x = joystick.Horizontal;
+        float z = joystick.Vertical;
+
+        // 2. Move Position 
+        moveVec = new Vector3(x, 0, z) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + moveVec);
+
+        if (moveVec.sqrMagnitude == 0)
+            return; // #. No input = No Rotation
+
+        // 3. Move Rotation
+        Quaternion dirQuat = Quaternion.LookRotation(moveVec);
+        Quaternion moveQuat = Quaternion.Slerp(rb.rotation, dirQuat, 0.3f);
+        rb.MoveRotation(moveQuat);
     }
+
+
+
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
